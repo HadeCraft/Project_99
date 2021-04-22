370609f1805ef4e3a6997d723875463537484882
Nirbhik <70087592+Nirbhik007@users.noreply.github.com>
2021-04-22 09:25:55 +0530

    Add files via upload

---
+++ Project-99/Removefiles.py
@@ -0,0 +1,15 @@
+import time
+import os
+import shutil
+
+path = path
+days = time.time()
+
+os.path.exists(path)
+os.walk(path)
+os.path.join()
+os.stat(path)
+
+ctime = os.stat(path).st_ctime
+
+return(ctime)
